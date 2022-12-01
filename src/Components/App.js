6edf7ba80b07d65cf55fa450446e494d8fa5082f
seeratbhatia <b.seerat@gmail.com>
2022-12-01 14:28:34 +0530

    Add files via upload

---
+++ src/Components/App.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+import Stopwatch from "./Stopwatch";
+import Countdown from "./Countdown";
+
+class App extends React.Component {
+  state = {
+    stopwatch: false,
+    countdown: false,
+  };
+  close = (key) => {
+    this.setState({ [key]: false });
+  };
+  render() {
+    return (
+      <div>
+        <div className="App-title">🕗 Timers 🕗 </div>
+        <div className="Timers">
+          {this.state.stopwatch ? (
+            <Stopwatch close={this.close} />
+          ) : (
+            <button onClick={() => this.setState({ stopwatch: true })}>
+              Show Stopwatch
+            </button>
+          )}
+          {this.state.countdown ? (
+            <Countdown close={this.close} />
+          ) : (
+            <button onClick={() => this.setState({ countdown: true })}>
+              Show Countdown
+            </button>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
